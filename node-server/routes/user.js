bd969d5532a098295c2e16deedf4683d2bd26b90
s3891667 <s3891667@rmit.edu.vn>
2023-02-23 14:59:49 +1100

    test

--- node-server/routes/user.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var axios = require("axios");
-var userControl = require("../middleWare/user/userControl.js");
-
-router.post("/signUp/", userControl.register);
-router.post("/login/", userControl.login);
-
-module.exports = router;
